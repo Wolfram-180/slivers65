df4131b7777af8a8994d950ebeb496efa933421f
Wolfram-180 <sergei.ulvis@gmail.com>
2023-06-07 07:00:41 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const _title = '';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
+      title: _title,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: ''),
+      home: const MyHomePage(title: _title),
     );
   }
 }
