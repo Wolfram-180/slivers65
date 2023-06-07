2a9e0b8dd23d4fbc16c9ba0e57d1c624cabe9455
Wolfram-180 <sergei.ulvis@gmail.com>
2023-06-07 09:46:40 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,33 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+/*       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-      ),
+      ), */
       body: Container(
         child: CustomScrollView(
           slivers: [
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              pinned: true,
+              title: Text(
+                widget.title,
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              expandedHeight: 150,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network(
+                  _imgUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
             SliverList(
               delegate: SliverChildListDelegate(
                 [
